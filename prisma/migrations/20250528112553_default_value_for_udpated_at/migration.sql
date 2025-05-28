137df44521899976cfd06865a56b35ebf06d12ec
Vivek205 <ichbinvivek@gmail.com>
2025-05-28 13:26:18 +0200

    fix: Set default value for updatedAt fields in User, Extension, and UserExtension models. Updated migration SQL to reflect changes in the database schema.

---
+++ prisma/migrations/20250528112553_default_value_for_udpated_at/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Extension" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "UserExtension" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
